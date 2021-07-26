25bfdd97206862b6343d82574adda6fd28ccd277
Bryan Gutierrez <gutierrezbryan32@yahoo.com>
2021-07-26 10:46:15 -0400

    commit tree 7dd21ba0c575eb83596710896b12ab38b08e654d

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    //console.log('After DOM Loaded')
+    document.getElementById('text')
+    .textContent = "This is really cool!";
+    //.innerHTML = "This is really cool!";
+    console.log(text)
+})
+    //console.log('Before DOM loads')
